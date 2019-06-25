ba429cefd184948ee148a338b530f23f31d0f4f4
douglas martins santos <douglas.santos@avecbrasil.com.br>
2019-06-25 15:58:23 -0300

    update route new

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import './Feed.css';
 
@@ -8,64 +9,52 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+    //ele serve para armazenar informações que quando ele for alterada ela reflete no body do html
+    state = {
+        feed: [],
+    };
+
+   //ele é montado de forma automatica quando o componente é montado em tela
+   async componentDidMount() {
+        // no post contém todos os dados dele que buscou e salvou no response
+        const response = await api.get('posts');
+
+        this.setState({ feed: response.data });
+   }
+
     render() {
         return (
            <section id="post-list">
-               <article>
-                   <header>
-                       <div className='user-info'>
-                           <span> Douglas Santos</span>
-                           <span className="palce">São paulo</span>
-                       </div>
-
-                       <img src={more} alt="mais" />
-                   </header>
-
-                   <img src="http://localhost:3333/files/t_rex_codes.jpg" alt="" />
-
-                   <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
+              { this.state.feed.map(post => (
+                   <article key={post._id}> 
+                    <header>
+                        <div className='user-info'>
+                            <span>{post.author}</span>
+                            <span className="palce">{post.place}</span>
                         </div>
 
-                        <strong>900 curtidas</strong>
+                        <img src={more} alt="mais" />
+                    </header>
 
-                        <p>
-                            Um post da T-Rex week
-                            <span>  #react  #trexweek  #top</span>
-                        </p>
-                   </footer>
+                    <img src= {`http://localhost:3333/files/${post.image}`} alt="" />
+
+                    <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
+
+                            <strong>{post.likes} curtidas</strong>
+
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                    </footer>
                </article>
+              )) }
 
-               <article>
-                   <header>
-                       <div className='user-info'>
-                           <span> Douglas Santos</span>
-                           <span className="palce">São paulo</span>
-                       </div>
-
-                       <img src={more} alt="mais" />
-                   </header>
-
-                   <img src="http://localhost:3333/files/t_rex_codes.jpg" alt="" />
-
-                   <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
-
-                        <strong>900 curtidas</strong>
-
-                        <p>
-                            Um post da T-Rex week
-                            <span>  #react  #trexweek  #top</span>
-                        </p>
-                   </footer>
-               </article>
            </section>
         );
     }
